19e649b7a028016c4e7dd381b103dfcbf06b8c62
jetbasrawi <anewexplorer@gmail.com>
2016-03-02 12:59:48 +0000

    Added comment to start_twittervotes.sh

--- start_twittervotes.sh
+++ start_twittervotes.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#These keys are no longer valid, but have been left in place so you can see the complete setup
 export SP_TWITTER_KEY=aWgjineptdAAeQLyv5HkmrL5z
 export SP_TWITTER_SECRET=tVefKaHmCEAvhCVoxr4kMrwZ7PTAHei5qgrneS1xVc2FEjNYrH
 export SP_TWITTER_ACCESSTOKEN=263885735-e8ATvjUPRA0hyLc6Q8Z33bcsc6kDIoXcs2PBfE7d
